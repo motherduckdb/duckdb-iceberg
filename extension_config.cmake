f6d1ef1a0a230494d3920343f904d343e9974c7f
Tmonster <tom@ebergen.com>
2025-08-06 09:11:55 +0200

    use avro reader

--- extension_config.cmake
+++ extension_config.cmake
@@ -17,8 +17,10 @@ duckdb_extension_load(ducklake
 
 duckdb_extension_load(avro
         LOAD_TESTS
-        GIT_URL https://github.com/duckdb/duckdb-avro
-        GIT_TAG b75cb5cea43d3e2bee6f5d0f377aa99354dd868a
+        INCLUDE_DIR /Users/tomebergen/git/duckdb-avro/src/include
+        SOURCE_DIR /Users/tomebergen/git/duckdb-avro
+#        GIT_URL https://github.com/duckdb/duckdb-avro
+#        GIT_TAG b75cb5cea43d3e2bee6f5d0f377aa99354dd868a
 )
 
 if (NOT EMSCRIPTEN)
