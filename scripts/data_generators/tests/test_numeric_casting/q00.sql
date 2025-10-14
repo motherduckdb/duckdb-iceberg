47d4e4a9563ce01506cd4435b36684f99da27fb7
Tmonster <tom@ebergen.com>
2025-10-14 12:56:40 +0200

    missed a file

--- scripts/data_generators/tests/test_numeric_casting/q00.sql
+++ scripts/data_generators/tests/test_numeric_casting/q00.sql
@@ -1,10 +1,6 @@
-CREATE OR REPLACE TABLE default.insert_all_types (
+CREATE OR REPLACE TABLE default.test_numeric_casting (
 	byte_col TINYINT,
 	short_col SMALLINT,
 	int_col INT,
-	long_col BIGINT,
-	float_col FLOAT,
-	double_col DOUBLE,
-	decimal_col DECIMAL(15, 5),
-	date_col DATE
+	long_col BIGINT
 ) USING ICEBERG;
