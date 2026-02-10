5f05cf43e01905c5f39e52fc392ab1455b32e56b
Tishj <t_b@live.nl>
2026-02-10 17:18:27 +0100

    update test, because Spark fails to do the ALTER SET DEFAULT

--- scripts/data_generators/tests/pyspark_iceberg_table_v2/q08.sql
+++ scripts/data_generators/tests/pyspark_iceberg_table_v2/q08.sql
@@ -1,2 +1,3 @@
-ALTER TABLE default.pyspark_iceberg_table_v2
-ALTER COLUMN schema_evol_added_col_1 TYPE BIGINT;
+UPDATE default.pyspark_iceberg_table_v2
+SET schema_evol_added_col_1 = 42
+WHERE schema_evol_added_col_1 IS NULL;
---
+++ scripts/data_generators/tests/pyspark_iceberg_table_v2/q09.sql
@@ -0,0 +1,1 @@
+CALL system.rewrite_data_files('default.pyspark_iceberg_table_v2');
