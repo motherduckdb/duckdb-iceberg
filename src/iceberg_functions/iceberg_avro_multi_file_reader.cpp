004976c6bb6af2c1e864734cb6298a8fce9bbc47
Tishj <t_b@live.nl>
2026-02-11 10:22:06 +0100

    first-row-id is only mandatory in manifest_file that has content DATA, DELETE doesn't need it

--- src/iceberg_functions/iceberg_avro_multi_file_reader.cpp
+++ src/iceberg_functions/iceberg_avro_multi_file_reader.cpp
@@ -473,6 +473,7 @@ void IcebergAvroMultiFileReader::FinalizeChunk(ClientContext &context, const Mul
 			continue;
 		}
 		first_row_id_validity.SetValid(i);
+		D_ASSERT(manifest_file.has_first_row_id);
 		first_row_id_data[i] = manifest_file.first_row_id + start_row_id;
 		start_row_id++;
 	}
