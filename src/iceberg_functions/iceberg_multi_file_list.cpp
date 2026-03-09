cc83ab179cce0be9e7bde1845e88de9abaa2ae22
peter <boncz@cwi.nl>
2026-03-09 18:54:43 +0100

    remove spurious change

--- src/iceberg_functions/iceberg_multi_file_list.cpp
+++ src/iceberg_functions/iceberg_multi_file_list.cpp
@@ -58,7 +58,7 @@ public:
 		}
 		try {
 			{
-				TaskNotifier task_notifier {state.context, "iceberg_read_manifest"};
+				TaskNotifier task_notifier {state.context};
 				auto res = TaskExecutionResult::TASK_NOT_FINISHED;
 				while (res == TaskExecutionResult::TASK_NOT_FINISHED) {
 					res = ExecuteTaskIncremental();
