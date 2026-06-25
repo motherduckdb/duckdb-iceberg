6db4b87ff442530fe6cc2246c60ffffeb7d3c068
Tishj <t_b@live.nl>
2026-06-25 22:59:09 +0200

    bump duckdb-avro hash

--- extension_config.cmake
+++ extension_config.cmake
@@ -2,7 +2,7 @@
 if (NOT EMSCRIPTEN)
   duckdb_extension_load(avro
   LOAD_TESTS
-  GIT_URL https://github.com/duckdb/duckdb_avro
+  GIT_URL https://github.com/duckdb/duckdb-avro
   GIT_TAG 1e1055ffc25dde1e3be2ca3ff2ef6218f4a4467d
   SUBMODULES "third_party/avro-c"
 )
