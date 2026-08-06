81f147902d12e1cf3a790804c6fc3a20a37ebac0
Tishj <t_b@live.nl>
2026-08-06 14:43:14 +0200

    parallelize reads within the same avro file, at the block level

--- extension_config.cmake
+++ extension_config.cmake
@@ -2,8 +2,8 @@
 if (NOT EMSCRIPTEN)
   duckdb_extension_load(avro
   LOAD_TESTS
-  GIT_URL https://github.com/duckdb/duckdb-avro
-  GIT_TAG 3dd16be1ac5153834d6bc88dbbc05807ce4276d7
+  GIT_URL https://github.com/tishj/duckdb_avro
+  GIT_TAG 3cac15462f91e5e9152bc32aaffba522b53c961f
   SUBMODULES "third_party/avro-c"
 )
 endif()
