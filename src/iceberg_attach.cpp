37f7cefbecc19a99e4f898652a16b74851983999
Tishj <t_b@live.nl>
2026-08-06 18:16:26 +0200

    fix error message

--- src/iceberg_attach.cpp
+++ src/iceberg_attach.cpp
@@ -319,7 +319,7 @@ unique_ptr<Catalog> IcebergAttach::Attach(optional_ptr<StorageExtensionInfo> sto
 	catalog->GetConfig(context, endpoint_type);
 	if (!default_schema.empty() &&
 	    !IRCAPI::VerifySchemaExistence(context, *catalog, default_schema.GetIdentifierName())) {
-		throw InvalidConfigurationException("default_schema %s does not exist", default_schema);
+		throw InvalidConfigurationException("default_schema '%s' does not exist", default_schema);
 	}
 	return std::move(catalog);
 }
